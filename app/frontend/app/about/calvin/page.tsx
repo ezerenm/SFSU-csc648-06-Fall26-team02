4e8d979e16838b6abbeedfea0e67b7bc36897817
Kaolinium <calvinmei.academics@gmail.com>
2026-09-14 22:40:54 -0700

    Added Calvin's personal page

---
+++ app/frontend/app/about/calvin/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function CalvinsPage() {
+    return (
+        <main className="min-h-screen bg-black text-yellow flex flex-col items-center justify-center">
+            <h1>Calvin Mei</h1>
+            <p>cmei@sfsu.edu</p>
+            <p>Team 02</p>
+            <p>Front-End Master</p>
+
+            <Image
+                src="/CM_CINE440-1.png"
+                alt="Calvin Mei"
+                width={176}
+                height={124}
+            />
+                
+        </main>
+    );
+}
